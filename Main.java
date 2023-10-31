bda0b3dc25a7a6849c57683edb7e3a8af2e34aa5
parassunijuniarti <146043931+parassunijuniarti@users.noreply.github.com>
2023-10-31 02:43:55 -0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,27 @@
 package com.tutorial;
 
-public class Main {
+class Mahasiswa {
+String nama;
+String nim;
+String prodi;
+}
 
+
+public class Main {
     public static void main(String[] args) {
+        //instasiasi objek dari calss Mahasiswa
+        Mahasiswa mhs1 = new Mahasiswa();//
+        //memberi atribut pada objek mhs1
+        mhs1.nama = "Paras Suni Juniarti";
+        mhs1.nim = "22241049";
+        mhs1.prodi = "PTI";
+
+
+        System.out.println("Nama : " + mhs1.nama);
+        System.out.println("NIM : " + mhs1.nim);
+        System.out.println("Prodi : " + mhs1.prodi);
+
+        
         
     }
 }
