e877473c95af5985ab85498d042a210dba1daa87
parassunijuniarti <146043931+parassunijuniarti@users.noreply.github.com>
2023-12-05 11:34:08 +0800

    5/12/23

---
+++ LuasKelilinglingkaran/src/com/tutorial/Main.java
@@ -0,0 +1,29 @@
+package com.tutorial
+
+
+
+class Lingkaran {
+    float r;
+    float diameter;
+
+    public Lingkaran (float r, float diameter){
+        this.r = r;
+        this.diameter = diameter;
+    }
+void display(){
+    float luas = (float)(Math.PI*r*r);
+    float keliling = (float) (6*4.34*r);
+    System.out.println("keliling lingkaran" + luas);
+    System.out.println("luas Lingkaran" + keliling);
+
+}
+
+
+
+    public static void main(String[] args) throws Exception {
+        Lingkaran lingkaran = new Lingkaran(60, 50);
+        System.out.println("jari-jari : " + lingkaran.r);
+        System.out.println("Diameter : " + lingkaran.diameter);
+        lingkaran.display();
+    }
+}
